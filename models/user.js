59c7f1591a5ddc934880bf918d85d0df56583077
Toby Moore <toby@Tobys-MacBook-Pro.local>
2022-08-04 12:11:06 +0100

    added validation

--- models/user.js
+++ models/user.js
@@ -43,6 +43,7 @@ const validate = (data) => {
         lastName: Joi.string().required().label('Last Name'),
         email: Joi.string().email().required().label('Email'),
         password: passwordComplexity().required().label('Password'),
+        location: Joi.string().required().label('Location'),
     });
     return schema.validate(data);
 };
